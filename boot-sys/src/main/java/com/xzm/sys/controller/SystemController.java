948664e48a8744a508a51bdc16fa74c08c0e6bec
xuzongmeng <1102207843@qq.com>
2018-07-19 13:41:12 +0800

    修改工程层级 统一管理维护项目版本

--- boot-sys/src/main/java/com/xzm/sys/controller/SystemController.java
+++ boot-sys/src/main/java/com/xzm/sys/controller/SystemController.java
@@ -1,4 +1,4 @@
-package com.xzm.system.controller;
+package com.xzm.sys.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
