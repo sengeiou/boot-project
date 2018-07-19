adfce05881f1fbf29f6fcd583e61832cb02eb5b6
xuzongmeng <1102207843@qq.com>
2018-07-19 10:52:02 +0800

    xxx

---
+++ boot-xhb/src/main/java/com/xzm/sys/SysController.java
@@ -0,0 +1,9 @@
+package com.xzm.sys;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "sys")
+public class SysController {
+}
