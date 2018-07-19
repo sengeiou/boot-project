8a69978e86524facdd67a3f0d5f837889dd6ba37
xuzongmeng <1102207843@qq.com>
2018-07-19 11:41:54 +0800

    xxxxx

---
+++ boot-jdt/src/main/java/com/xzm/jdt/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.xzm.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping(value = "web")
+public class WebController {
+    @RequestMapping(value = "/index")
+    public String index(HttpServletRequest request) {
+        return "访问到了===WebController";
+    }
+}
