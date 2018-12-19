95c70e774f66a7bf1ee7cf0c3a2c5ff1ea301530
xuzongmeng <1102207843@qq.com>
2018-12-19 22:18:34 +0800

    xxxxxxx

---
+++ boot-single/src/test/java/com/xzm/Test.java
@@ -0,0 +1,33 @@
+package com.xzm;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Test {
+    private static int msgCount = 0; //消息发送次数
+    private static Timer mTimer;
+    public static void main(String[] args) {
+        // 获得一个给定范围的随机整数
+//        int num = getRandomNum(-1, 2);
+//        System.out.println("====" + num);、
+        startTask();
+    }
+    public static int getRandomNum(int smallistNum, int BiggestNum) {
+        Random random = new Random();
+        return (Math.abs(random.nextInt()) % (BiggestNum - smallistNum + 1))+ smallistNum;
+    }
+
+    //每秒发送一条消息
+    private static void startTask() {
+        mTimer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                int num = getRandomNum(-1, 2);
+                System.out.println("====" + num);
+            }
+        };
+        mTimer.schedule(timerTask, 0, 1000);
+    }
+}
---
+++ boot-single/src/test/java/com/xzm/mapper/UserMapperTest.java
@@ -0,0 +1,144 @@
+//package com.xzm.mapper;
+//
+//
+//import com.xzm.modules.test.dao.UserMapper;
+//import com.xzm.modules.test.po.User;
+//import com.xzm.modules.test.po.UserCustom;
+//import com.xzm.modules.test.po.UserQueryVo;
+//import org.apache.ibatis.io.Resources;
+//import org.apache.ibatis.session.SqlSession;
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.io.InputStream;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * Created by 10412 on 2016/12/2.
+// */
+//public class UserMapperTest
+//{
+//    private SqlSessionFactory sqlSessionFactory;
+//
+//
+//    //此方法是在 testFindUserById 方法之前执行的
+//    @Before
+//    public void setup() throws Exception
+//    {
+//        //创建sqlSessionFactory
+//
+//        //Mybatis 配置文件
+//        String resource = "SqlMapConfig.xml";
+//
+//        //得到配置文件流
+//        InputStream inputStream = Resources.getResourceAsStream(resource);
+//
+//        //创建会话工厂,传入Mybatis的配置文件信息
+//        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//    }
+//
+//    @Test
+//    public void testFindUserById() throws Exception
+//    {
+//        SqlSession sqlSession = sqlSessionFactory.openSession();
+//
+//        //创建usermapper对象,mybatis自动生成代理对象
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//
+//        //调用UserMapper的方法
+//        User user = userMapper.findUserById(1);
+//
+//
+//        System.out.println(user);
+//    }
+//
+//    @Test
+//    public void testFindUserByUsername() throws Exception
+//    {
+//        SqlSession sqlSession = sqlSessionFactory.openSession();
+//
+//        //创建usermapper对象,mybatis自动生成代理对象
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//
+//        //调用UserMapper的方法
+//        List<User> list = userMapper.findUserByUsername("张小明");
+//
+//        sqlSession.close();
+//
+//        System.out.println(list);
+//    }
+//
+//    //测试用户信息综合查询
+//    @Test
+//    public void testFindUserList() throws Exception
+//    {
+//        SqlSession sqlSession = sqlSessionFactory.openSession();
+//
+//        //创建usermapper对象,mybatis自动生成代理对象
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//
+//        //创建包装对象，设置查询条件
+//        UserQueryVo userQueryVo = new UserQueryVo();
+//        UserCustom userCustom = new UserCustom();
+//        //因为设置了动态的sql，如果不设置某个值，那么条件就不会拼接在sql上
+//        userCustom.setSex("男");
+//        //userCustom.setUsername("张小明");
+//
+//        //传入多个id
+//        List<Integer> ids = new ArrayList<>();
+//        ids.add(1);
+//        ids.add(10);
+//        ids.add(16);
+//        userQueryVo.setIds(ids);
+//        userQueryVo.setUserCustom(userCustom);
+//
+//        //调用UserMapper的方法
+//        List<UserCustom> list = userMapper.findUserList(userQueryVo);
+//
+//        System.out.println(list);
+//    }
+//
+//
+//
+//    //测试用户信息综合查询总数
+//    @Test
+//    public void testFindUserCount() throws Exception
+//    {
+//        SqlSession sqlSession = sqlSessionFactory.openSession();
+//
+//        //创建usermapper对象,mybatis自动生成代理对象
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//
+//        //创建包装对象，设置查询条件
+//        UserQueryVo userQueryVo = new UserQueryVo();
+//        UserCustom userCustom = new UserCustom();
+//        userCustom.setSex("男");
+//        userCustom.setUsername("张小明");
+//        userQueryVo.setUserCustom(userCustom);
+//
+//        //调用UserMapper的方法
+//
+//        System.out.println(userMapper.findUserCount(userQueryVo));
+//    }
+//
+//
+//    //测试根据id查询用户信息，使用 resultMap 输出
+//    @Test
+//    public void testFindUserByIdResultMap() throws Exception
+//    {
+//        SqlSession sqlSession = sqlSessionFactory.openSession();
+//
+//        //创建usermapper对象,mybatis自动生成代理对象
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//
+//        //调用UserMapper的方法
+//        User user = userMapper.findUserByIdResultMap(1);
+//
+//
+//        System.out.println(user);
+//    }
+//
+//}
