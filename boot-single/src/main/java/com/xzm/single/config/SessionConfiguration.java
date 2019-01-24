8c29dd68e017609503118413aa3502579ac60034
xuzongmeng <1102207843@qq.com>
2019-01-24 11:55:00 +0800

    JDT修改

--- boot-single/src/main/java/com/xzm/single/config/SessionConfiguration.java
+++ boot-single/src/main/java/com/xzm/single/config/SessionConfiguration.java
@@ -1,5 +1,6 @@
 package com.xzm.single.config;
 
+import com.xzm.single.common.interceptor.SessionInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -19,7 +20,7 @@ public class SessionConfiguration extends WebMvcConfigurerAdapter
 {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-//       registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**");
+      registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**");
 //       registry.addInterceptor(new HandshakeInterceptor());
     }
 
