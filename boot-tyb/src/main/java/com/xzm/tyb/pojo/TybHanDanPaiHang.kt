bae472a8707ae0cceada53bd4767293aaae0ff21
xuzongmeng <1102207843@qq.com>
2019-01-24 10:28:15 +0800

    JDT修改

---
+++ boot-tyb/src/main/java/com/xzm/tyb/pojo/TybHanDanPaiHang.kt
@@ -0,0 +1,15 @@
+package com.xzm.tyb.pojo
+
+import java.util.Date
+
+//@Data
+class TybHanDanPaiHang {
+    var id: Int? = null
+    var title: String? = null
+    var producttype: String? = null
+    var ordertime: String? = null
+    var point: String? = null
+    var profit: String? = null
+    var createTime: Date? = null
+    var updataTime: Date? = null
+}
