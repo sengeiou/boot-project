15bb085fce36bd47077ccc500876c1490b535d41
xuzongmeng <1102207843@qq.com>
2019-01-28 18:02:53 +0800

    JDT修改

---
+++ boot-tyb/src/main/java/com/xzm/tyb/pojo/TybUserKaiHu.kt
@@ -0,0 +1,23 @@
+package com.xzm.tyb.pojo
+
+import java.util.Date
+
+//@Data
+class TybUserKaiHu {
+    var id: Long = 0
+
+    var userName: String? = null
+
+    var idCard: String? = null
+
+    var phone: String? = null
+
+    var platformCode: String? = null
+
+    var platformName: String? = null
+
+    var createTime: Date? = null
+
+    var updataTime: Date? = null
+
+}
---
+++ boot-tyb/src/main/java/com/xzm/tyb/pojo/liveTop.kt
@@ -0,0 +1,33 @@
+package com.xzm.tyb.pojo
+
+import java.util.Date
+
+class liveTop {
+     var id: Long = 0
+
+    var title: String? = null
+
+    var liveUrl: String? = null
+
+    var dianZanCount: Int? = null
+
+    var adText: String? = null
+
+    var adUrl: String? = null
+
+    var onlineCount: Int? = null
+
+    var handanRate: Float? = null
+
+    var yingLi: Float? = null
+
+    var mainImg: String? = null
+
+    var subImg: String? = null
+
+    var createTime: Date? = null
+
+    var updataTime: Date? = null
+
+
+}
